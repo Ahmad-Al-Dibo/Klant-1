23a2ac2fd8a3549d8c7da5fd924bef8aa69129f9
CodeHub developer <ahmad.dibo@student.graafschapcollege.nl>
2025-12-10 08:22:54 +0100

    Delete backend directory

--- backend/config/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-from .development import *
-
-try:
-    from .local import *  # noqa: F401
-except ImportError:
-    pass
--- backend/core/urls.py
+++
@@ -1,8 +0,0 @@
-
-
-from django.urls import include, path
-from .views import home_view
-
-urlpatterns = [
-    path('home', home_view, name='home')
-]
