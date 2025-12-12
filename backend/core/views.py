22001f1b9512f322033157b55d360bde1b5f9edc
CodeHub developer <ahmad.dibo@student.graafschapcollege.nl>
2025-12-12 09:55:33 +0100

    Update views.py
    Adding Api future to create user using Api

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .models import CustomUser
+from .serializers import UserSerializer
 
 def home(request):
     return render(request, 'core/home.html')
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
